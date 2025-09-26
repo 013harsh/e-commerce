ae7313eb110d151926a0537d3947acb960691ee5
Harsh Kumar <youremail@example.com>
2025-09-26 10:59:17 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,27 +14,26 @@ const Home = () => {
         </div>
       </nav>
       <nav className="w-full h-full p-3">
-        <div className="flex flex-wrap items-center justify-center w-full h-full p-2 bg-zinc-100 ">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="flex items-center justify-start w-full h-full gap-5 px-10 py-5 overflow-x-scroll scrollbar-hide flex-nowrap bg-zinc-100 ">
           <Card />
         </div>
       </nav>
       <nav className="w-full h-[400px] ">
         <p className="flex items-center justify-between w-full px-5 font-bold">
           <span className="text-[23px]">Trending Product</span>
+          <span className="">See More</span>
         </p>
       </nav>
-      <nav className="w-full h-[400px] border border-black">
-      <p className="flex items-center justify-between w-full px-5 font-bold">
+      <nav className="w-full h-[400px]">
+        <p className="flex items-center justify-between w-full px-5 font-bold">
           <span className="text-[23px]">Kid's</span>
+          <span className="">See More</span>
         </p>
-        </nav> 
-      <nav className="w-full h-[400px] border border-black">
-      <p className="flex items-center justify-between w-full px-5 font-bold">
+      </nav>
+      <nav className="w-full h-[400px]">
+        <p className="flex items-center justify-between w-full px-5 font-bold">
           <span className="text-[23px]">Games</span>
+          <span className="">See More</span>
         </p>
       </nav>
     </div>
