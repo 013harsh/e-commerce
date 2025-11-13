01704e5ecd34600f0c91c1ed3ff6735b7182a634
Harsh Kumar <167132722+013harsh@users.noreply.github.com>
2025-11-13 16:14:00 +0530

    .git

--- frontend/src/routes/MainRoutes.jsx
+++ frontend/src/routes/MainRoutes.jsx
@@ -2,8 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Cart from "../pages/Cart";
 import Account from "../pages/Account";
-import Categories from "../pages/Categories";
-import SearchBar from "../pages/SearchBar";
+// import Categories from "../pages/Categories";
+// import SearchBar from "../pages/SearchBar";
 import PageNotFound from "../pages/PageNotFound";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
@@ -16,8 +16,8 @@ const MainRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/account" element={<Account />} />
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/search" element={<SearchBar />} />
+      {/* <Route path="/categories" element={<Categories />} /> */}
+      {/* <Route path="/search" element={<SearchBar />} /> */}
       <Route path="*" element={<PageNotFound />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
