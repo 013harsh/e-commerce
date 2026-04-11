2ace94eeacf085dfca3903ce0048bbea37b3e4da
Harsh Kumar <167132722+013harsh@users.noreply.github.com>
2026-04-11 10:58:10 +0530

    update

--- frontend/src/api/appwriteConfig.jsx
+++ frontend/src/api/appwriteConfig.jsx
@@ -1,15 +1,17 @@
 import { Client, Databases, Account, Storage, ID } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("https://sgp.cloud.appwrite.io/v1") // Appwrite Cloud endpoint
-  .setProject("69cb53bb000088490e62"); // Appwrite Project ID
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const storage = new Storage(client);
-export { ID } from "appwrite";
 
-export const DATABASE_ID = "69cb5cf5000781a016d4";
-export const PRODUCTS_COLLECTION_ID = "69cb64af002e88e33193";
-export const CART_COLLECTION_ID = "69cb64a40019430e6053";
-export const IMAGES_BUCKET_ID = "69cd5011001b9e719d92";
+export const DATABASE_ID = import.meta.env.VITE_DATABASE_ID;
+export const PRODUCTS_COLLECTION_ID = import.meta.env
+  .VITE_PRODUCTS_COLLECTION_ID;
+export const CART_COLLECTION_ID = import.meta.env.VITE_CART_COLLECTION_ID;
+export const IMAGES_BUCKET_ID = import.meta.env.VITE_IMAGES_BUCKET_ID;
+
+export { ID };
